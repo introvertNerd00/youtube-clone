6edeb698c3977329923df2a6b481f197bd980936
Hamza <hamza.afzalkhan9@gmail.com>
2023-08-12 00:13:53 +0500

    added API ChannelCard VideoCard component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,21 +1,26 @@
 import {useState,useEffect}from 'react';
 import{Box,Typography,Stack} from '@mui/material';
-import Sidebar from './Sidebar';
+import{Videos,Sidebar} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+    const [selectedCategory,setSelectedCategory]=useState('New');
+    useEffect(() => {fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data)=>setVideos(data.items))},[selectedCategory]);
+    const [videos,setVideos]=useState([]);
   return (
     <Stack sx={{flexDirection:{sx:"column", md:"row"}}}>
         <Box sx={{height:{sx:'auto', md:'92vh'}, borderRight:'1px solid #3d3d3d', px:{sx:0, md:2}}}>
-            <Sidebar/>
+            <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
             <Typography className='copyright' variant='body2' sx={{ mt:1.5, color:'#fff'}}>
                 Copyright 2023 HAKX Media
             </Typography>
         </Box>
-        <Box>
+        <Box p={2} sx={{overflowY:'auto', height:'90vh', flex:2}}>
             {/* <VideoDetail/> */}
-            <Typography>
-                New<span></span>
+            <Typography variant='h4' fontWeight='bold' mb={2} sx={{color:'white'}}>
+                {selectedCategory} <span style={{color:'#F31503'}}>Videos</span>
             </Typography>
+            <Videos videos={videos}/>
         </Box>
     </Stack>
 
